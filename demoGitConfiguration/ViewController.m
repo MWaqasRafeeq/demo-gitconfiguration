9560085041e0b2f63d4f8d1ca7ccce3e16e7eb74
Qubit LTD <qubitpvtltd@Qubits-MacBook-Pro.local>
2018-11-03 13:33:54 +0500

    add a commit in view controller file for just testing

--- demoGitConfiguration/ViewController.m
+++ demoGitConfiguration/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // add a commit in view controller file for just testing 
 }
 
 
